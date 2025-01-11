5b2dc08151be0718e618f450db5d2f90e7427668
vicmmoraM <vicmmora@espol.edu.ec>
2025-01-11 17:23:59 -0500

    Correción de findMax

--- src/modelo/CTest.java
+++ src/modelo/CTest.java
@@ -4,7 +4,12 @@ import org.junit.Test;
 public class CTest {
 	@Test
 	public void testFindMax() {
-        assertEquals(4, Calculation.findMax(new int[]{1, 3, 4, 2}));
+		//Con numeros positivos 
+		assertEquals(4, Calculation.findMax(new int[]{1, 3, 4, 2}));
+		//Con numeros negativos
         assertEquals(-1, Calculation.findMax(new int[]{-12, -1, -3, -4, -2}));
+        //Con numeros negativos y positivos 
+        assertEquals(15, Calculation.findMax(new int[]{0, 15, 10, 5, -5}));
+
     }
 }
--- src/modelo/Calculation.java
+++ src/modelo/Calculation.java
@@ -1,14 +1,15 @@
 package modelo;
 
 public class Calculation {
-	
-	public static int findMax(int arr[]) {
-        int max = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-        }
-        return max;
-	}
+	//Corección
+	 public static int findMax(int arr[]) {
+	        // Inicializar max con el primer elemento del arreglo
+	        int max = arr[0];
+	        for (int i = 1; i < arr.length; i++) {
+	            if (max < arr[i]) {
+	                max = arr[i];
+	            }
+	        }
+	        return max;
+	    }
 }
