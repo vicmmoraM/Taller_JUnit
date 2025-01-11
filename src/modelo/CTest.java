5fca233148a78bf9c6eacd5c068b5c47f0af2cd6
vicmmoraM <vicmmora@espol.edu.ec>
2025-01-11 17:02:22 -0500

    Primera parte - Con error en findMax

---
+++ src/modelo/CTest.java
@@ -0,0 +1,10 @@
+package modelo;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class CTest {
+	@Test
+	public void testFindMax() {
+        assertEquals(4, Calculation.findMax(new int[]{1, 3, 4, 2}));
+        assertEquals(-1, Calculation.findMax(new int[]{-12, -1, -3, -4, -2}));
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Taller_JUnit {
+	requires junit;
+}
